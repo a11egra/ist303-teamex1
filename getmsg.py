b25344121f6b11222d8e57034a971b84e3e166dd
allegra <agf.allegra@gmail.com>
2025-10-16 20:57:20 -0700

    final touches to make this function match adiyas

--- getmsg.py
+++ getmsg.py
@@ -1,15 +1,23 @@
+# Team Kalos, problem 3.1
+# Allegra's task: implement send_msg
 
 import datetime
 
 def get_msg(id:int):
+    '''
+    Finds message info based on message ID.
+    Checks if current time meets the wait time criteria for the message.
+    Returns message and True if the wait time is fulfilled, else returns
+       error message and False.
+    '''
 
     # make sure the id points to a real msg
     if id in message_dict:
-        msg_info = message_dict[id] # will return {msg : messagestring, open_time : datetime}
+        msg_info = message_dict[id] # will return {msg : messagestring, unlock_time : datetime}
 
         rn = datetime.now()
 
-        if rn >= msg_info["delay"]:
+        if rn >= msg_info["unlock_time"]:
             print(msg_info["msg"])
             return True
         else:
@@ -19,6 +27,3 @@ def get_msg(id:int):
     # if the id does not exist in the dictionary, kill the program
     else: 
         raise KeyError("There is no message associated with the ID provided.")
-
-
-
